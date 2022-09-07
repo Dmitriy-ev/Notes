37a97de22fa3902f744bdf4d4e8770c3cec4dd78
dmitriyy.ev@gmail.com <SpD34$otf$>
2022-09-07 17:51:47 +0300

    Initial commit

--- app/src/main/java/com/dmitry/notes/model/AppNote.kt
+++ app/src/main/java/com/dmitry/notes/model/AppNote.kt
@@ -3,6 +3,7 @@ package com.dmitry.notes.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity(tableName = "notes_tables")
 data class AppNote(
@@ -12,6 +13,4 @@ data class AppNote(
     val name: String = "",
     @ColumnInfo
     val text: String = "",
-
-
-)
+): Serializable
--- app/src/main/java/com/dmitry/notes/screens/main/MainFragment.kt
+++ app/src/main/java/com/dmitry/notes/screens/main/MainFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.dmitry.notes.R
 import com.dmitry.notes.databinding.FragmentMainBinding
+import com.dmitry.notes.model.AppNote
 import com.dmitry.notes.utilits.APP_ACTIVITY
 
 class MainFragment : Fragment() {
@@ -17,7 +20,9 @@ class MainFragment : Fragment() {
         get() = _binding ?: throw RuntimeException("FragmentMainBinding == null")
 
     private lateinit var mainFragmentViewModel: MainFragmentViewModel
-
+    private lateinit var mainFragmentRecyclerView: RecyclerView
+    private lateinit var adapter: MainAdapter
+    private lateinit var observerList: Observer<List<AppNote>>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
@@ -30,16 +35,32 @@ class MainFragment : Fragment() {
     }
 
     private fun initialization() {
-
+        adapter = MainAdapter()
+        mainFragmentRecyclerView = binding.recyclerView
+        mainFragmentRecyclerView.adapter = adapter
+        observerList = Observer {
+            val list = it.asReversed()
+            adapter.setList(list)
+        }
         mainFragmentViewModel = ViewModelProvider(this)[MainFragmentViewModel::class.java]
+        mainFragmentViewModel.allNotes.observe(this, observerList)
         binding.btnAddNote.setOnClickListener {
             APP_ACTIVITY.mNavController.navigate(R.id.action_mainFragment_to_addNewNoteFragment)
         }
-
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        mainFragmentViewModel.allNotes.removeObserver(observerList)
+        mainFragmentRecyclerView.adapter = null
+    }
+
+    companion object{
+        fun click(note: AppNote){
+            val bundle = Bundle()
+            bundle.putSerializable("note", note)
+            APP_ACTIVITY.mNavController.navigate(R.id.action_mainFragment_to_noteFragment, bundle)
+        }
     }
 }
